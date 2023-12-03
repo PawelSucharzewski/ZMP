05be7b8aa1e8ee7372955080059fb13a8419059b
PawelSucharzewski <259307@student.pwr.edu.pl>
2023-12-03 17:32:48 +0100

    added therads

--- ZMP_LAB2/zalazek/inc/LibInterface.hh
+++ ZMP_LAB2/zalazek/inc/LibInterface.hh
@@ -1,25 +1,20 @@
 #ifndef  LIBINTERFACE_HH
 #define  LIBINTERFACE_HH
-#include "AbstractInterp4Command.hh"
-#include <string.h>
-#include <dlfcn.h>
+
 #include <iostream>
+#include <dlfcn.h>
+#include <cassert>
+#include "AbstractInterp4Command.hh"
 
-class LibInterface {
-	public:
-		~LibInterface(){};
-		LibInterface(){};
-		bool init(std::string lib_name);
+class LibInterface{
+private:
+    void *_LibHandler;
+    std::string _CmdName;
+    AbstractInterp4Command *(*_pCreateCmd)(void);
 
-		std::string _CmdName();
-
-		void* _LibHandler;
-		const char *(*_pGetCmdName)(void);
-		AbstractInterp4Command *(*_pCreateCmd)(void);
-		AbstractInterp4Command *(*_pPrintCmd)(void);//mobileObj zamienić na scene kiedyś tam
-		AbstractInterp4Command *(*_pReadParams)(std::istream&);
-
-
+public:
+    ~LibInterface();
+    bool init(std::string libraryName);
+    AbstractInterp4Command *CreateCmd();
 };
-
 #endif
